e2b0b9ccd51bf47ea21906e3e24b6e487fbf3c7b
Hasan Alperen <78741582+alperen-selcuk@users.noreply.github.com>
2022-04-16 23:14:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "hello world from ITU IEEE RAS"
+    return "good bye ITU IEEE RAS"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="5000")
